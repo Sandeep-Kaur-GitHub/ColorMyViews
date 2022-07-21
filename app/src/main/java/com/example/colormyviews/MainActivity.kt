76b05d1d185ebb16ac9aa4d874a6c7d53e4c3fa2
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-07-21 20:51:44 +0530

    Change the background color by clicking on the views

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.colormyviews
 
+import android.graphics.Color
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+            setListener()
+    }
+    private fun setListener(){
+        binding.apply {
+            val clickAble: List<View> = listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText)
+            for (i in clickAble){
+                i.setOnClickListener(View.OnClickListener {
+                    changeColor(it)
+                })
+            }
+        }
+    }
+    private fun changeColor(view: View)
+    {
+        binding.apply {
+            when (view) {
+                boxOneText-> view.setBackgroundColor(Color.DKGRAY)
+                boxTwoText->view.setBackgroundColor(Color.BLUE)
+                boxThreeText->view.setBackgroundColor(Color.GREEN)
+                boxFourText->view.setBackgroundColor(Color.MAGENTA)
+                boxFiveText-> view.setBackgroundColor(Color.YELLOW)
+            }
+        }
     }
 }
